145155a660b356a942bc47e9514555af2f43cffd
BuiTuanVu <vubui2010@gmail.com>
2019-10-15 21:23:24 +0700

    Create ORM sequenlize

--- utils/db.js
+++ utils/db.js
@@ -13,3 +13,16 @@ sequelize
 
 module.exports = sequelize
 
+module.exports = {
+    createUser: async ({ name, password }) => {
+        return await User.create({ name, password })
+    },
+
+    Users: async () => {
+        return await User.findAll();
+    },
+
+    getUser: async obj => {
+        return await User.findOne({ where: obj, });
+    },
+}
